770b5011833f6cabb6849aa01db40bf1d618ff58
Nasim <mdnasimhosen333@gmail.com>
2025-04-06 23:32:01 +0600

    test properly

--- arithmatic.c
+++ arithmatic.c
@@ -4,13 +4,13 @@ int main()
     int a = 20;
     int b = 8;
     int sum = a + b;
-    int diff = a - b;
-    int prod = a * b;
+    int sub = a - b;
+    int mul = a * b;
     int div = a / b;
     int mod = a % b;
     printf("Sum: %d\n", sum);
-    printf("Difference: %d\n", diff);
-    printf("Product: %d\n", prod);
+    printf("Substraction: %d\n", sub);
+    printf("Multiplication: %d\n", mul);
     printf("Division: %d\n", div);
     printf("Modulus: %d\n", mod);
     return 0;
