379550cd3ffff8f7c602d9c6bffa00e5e285c9e1
Nasim <mdnasimhosen333@gmail.com>
2025-04-06 23:25:43 +0600

    learn sum

---
+++ arithmatic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+   int a = 20;
+   int b = 8;
+   int sum = a + b;
+   int diff = a - b;
+    int prod = a * b;
+    int div = a / b;
+    int mod = a % b;
+    printf("Sum: %d\n", sum);
+    // printf("Difference: %d\n", diff);
+    // printf("Product: %d\n", prod);
+    // printf("Division: %d\n", div);
+    // printf("Modulus: %d\n", mod);
+    return 0;
+}
