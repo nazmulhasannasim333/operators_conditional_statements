62641ee9f8ae1d98f495e648f6d1f1eace4d62ca
Nasim <mdnasimhosen333@gmail.com>
2025-04-06 23:26:23 +0600

    learn diff

--- arithmatic.c
+++ arithmatic.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-   int a = 20;
-   int b = 8;
-   int sum = a + b;
-   int diff = a - b;
+    int a = 20;
+    int b = 8;
+    int sum = a + b;
+    int diff = a - b;
     int prod = a * b;
     int div = a / b;
     int mod = a % b;
     printf("Sum: %d\n", sum);
-    // printf("Difference: %d\n", diff);
+    printf("Difference: %d\n", diff);
     // printf("Product: %d\n", prod);
     // printf("Division: %d\n", div);
     // printf("Modulus: %d\n", mod);
