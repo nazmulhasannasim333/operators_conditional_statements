ccf8e8c374ecf98347935b0164fbc72a83136369
Nasim <mdnasimhosen333@gmail.com>
2025-04-07 21:12:42 +0600

    learn if else statement

---
+++ if_else.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if(a>= 10){
+        printf("the number is greater than or equal to 10\n");
+    }else{
+        printf("there is no condition met\n");
+    }
+    return 0;
+}
